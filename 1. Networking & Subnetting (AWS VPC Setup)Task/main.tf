8f019ce5c7ff78898b8c29aa57fd9dc494ec3f29
Sriyansh Gupta <116352112+Shre202002@users.noreply.github.com>
2025-12-05 21:04:45 +0530

    Edit the main file

--- 1. Networking & Subnetting (AWS VPC Setup)Task/main.tf
+++ 1. Networking & Subnetting (AWS VPC Setup)Task/main.tf
@@ -8,131 +8,131 @@ terraform {
   }
 }
 
-resource "aws_vpc" "vishnu_pandey_main" {
+resource "aws_vpc" "sriyansh_main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   
   tags = {
-    Name = "vishnu_pandey_main_vpc"
+    Name = "sriyansh_main_vpc"
   }
 }
 
-resource "aws_internet_gateway" "vishnu_pandey_igw" {
-  vpc_id = aws_vpc.vishnu_pandey_main.id
+resource "aws_internet_gateway" "sriyansh_igw" {
+  vpc_id = aws_vpc.sriyansh_main.id
   
   tags = {
-    Name = "vishnu_pandey_main_igw"
+    Name = "sriyansh_main_igw"
   }
 }
 
-resource "aws_eip" "vishnu_pandey_nat_eip" {
+resource "aws_eip" "sriyansh_nat_eip" {
   domain = "vpc"
   
   tags = {
-    Name = "vishnu_pandey_nat_eip"
+    Name = "sriyansh_nat_eip"
   }
 }
 
-resource "aws_nat_gateway" "vishnu_pandey_nat" {
-  allocation_id = aws_eip.vishnu_pandey_nat_eip.id
-  subnet_id     = aws_subnet.vishnu_pandey_public_1.id
+resource "aws_nat_gateway" "sriyansh_nat" {
+  allocation_id = aws_eip.sriyansh_nat_eip.id
+  subnet_id     = aws_subnet.sriyansh_public_1.id
   
   tags = {
-    Name = "vishnu_pandey_nat_gateway"
+    Name = "sriyansh_nat_gateway"
   }
   
-  depends_on = [aws_internet_gateway.vishnu_pandey_igw]
+  depends_on = [aws_internet_gateway.sriyansh_igw]
 }
 
-resource "aws_subnet" "vishnu_pandey_public_1" {
-  vpc_id                  = aws_vpc.vishnu_pandey_main.id
+resource "aws_subnet" "sriyansh_public_1" {
+  vpc_id                  = aws_vpc.sriyansh_main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
   
   tags = {
-    Name = "vishnu_pandey_public_subnet_1"
+    Name = "sriyansh_public_subnet_1"
     Type = "Public"
   }
 }
 
-resource "aws_subnet" "vishnu_pandey_public_2" {
-  vpc_id                  = aws_vpc.vishnu_pandey_main.id
+resource "aws_subnet" "sriyansh_public_2" {
+  vpc_id                  = aws_vpc.sriyansh_main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
   
   tags = {
-    Name = "vishnu_pandey_public_subnet_2"
+    Name = "sriyansh_public_subnet_2"
     Type = "Public"
   }
 }
 
-resource "aws_subnet" "vishnu_pandey_private_1" {
-  vpc_id            = aws_vpc.vishnu_pandey_main.id
+resource "aws_subnet" "sriyansh_private_1" {
+  vpc_id            = aws_vpc.sriyansh_main.id
   cidr_block        = "10.0.10.0/24"
   availability_zone = "ap-south-1a"
   
   tags = {
-    Name = "vishnu_pandey_private_subnet_1"
+    Name = "sriyansh_private_subnet_1"
     Type = "Private"
   }
 }
 
-resource "aws_subnet" "vishnu_pandey_private_2" {
-  vpc_id            = aws_vpc.vishnu_pandey_main.id
+resource "aws_subnet" "sriyansh_private_2" {
+  vpc_id            = aws_vpc.sriyansh_main.id
   cidr_block        = "10.0.11.0/24"
   availability_zone = "ap-south-1b"
   
   tags = {
-    Name = "vishnu_pandey_private_subnet_2"
+    Name = "sriyansh_private_subnet_2"
     Type = "Private"
   }
 }
 
-resource "aws_route_table" "vishnu_pandey_public_rt" {
-  vpc_id = aws_vpc.vishnu_pandey_main.id
+resource "aws_route_table" "sriyansh_public_rt" {
+  vpc_id = aws_vpc.sriyansh_main.id
   
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.vishnu_pandey_igw.id
+    gateway_id = aws_internet_gateway.sriyansh_igw.id
   }
   
   tags = {
-    Name = "vishnu_pandey_public_route_table"
+    Name = "sriyansh_public_route_table"
   }
 }
 
-resource "aws_route_table" "vishnu_pandey_private_rt" {
-  vpc_id = aws_vpc.vishnu_pandey_main.id
+resource "aws_route_table" "sriyansh_private_rt" {
+  vpc_id = aws_vpc.sriyansh_main.id
   
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.vishnu_pandey_nat.id
+    nat_gateway_id = aws_nat_gateway.sriyansh_nat.id
   }
   
   tags = {
-    Name = "vishnu_pandey_private_route_table"
+    Name = "sriyansh_private_route_table"
   }
 }
 
-resource "aws_route_table_association" "vishnu_pandey_public_1_assoc" {
-  subnet_id      = aws_subnet.vishnu_pandey_public_1.id
-  route_table_id = aws_route_table.vishnu_pandey_public_rt.id
+resource "aws_route_table_association" "sriyansh_public_1_assoc" {
+  subnet_id      = aws_subnet.sriyansh_public_1.id
+  route_table_id = aws_route_table.sriyansh_public_rt.id
 }
 
-resource "aws_route_table_association" "vishnu_pandey_public_2_assoc" {
-  subnet_id      = aws_subnet.vishnu_pandey_public_2.id
-  route_table_id = aws_route_table.vishnu_pandey_public_rt.id
+resource "aws_route_table_association" "sriyansh_public_2_assoc" {
+  subnet_id      = aws_subnet.sriyansh_public_2.id
+  route_table_id = aws_route_table.sriyansh_public_rt.id
 }
 
-resource "aws_route_table_association" "vishnu_pandey_private_1_assoc" {
-  subnet_id      = aws_subnet.vishnu_pandey_private_1.id
-  route_table_id = aws_route_table.vishnu_pandey_private_rt.id
+resource "aws_route_table_association" "sriyansh_private_1_assoc" {
+  subnet_id      = aws_subnet.sriyansh_private_1.id
+  route_table_id = aws_route_table.sriyansh_private_rt.id
 }
 
-resource "aws_route_table_association" "vishnu_pandey_private_2_assoc" {
-  subnet_id      = aws_subnet.vishnu_pandey_private_2.id
-  route_table_id = aws_route_table.vishnu_pandey_private_rt.id
+resource "aws_route_table_association" "sriyansh_private_2_assoc" {
+  subnet_id      = aws_subnet.sriyansh_private_2.id
+  route_table_id = aws_route_table.sriyansh_private_rt.id
 }
